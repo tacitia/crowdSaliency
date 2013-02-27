667894c0f1c089e777339e8e8e207c9ae110fc7a
tacitia <lillian.g621@gmail.com>
2013-02-26 21:04:47 -0500

    z

--- googleMap/php/storeActionData.php
+++ googleMap/php/storeActionData.php
@@ -32,7 +32,7 @@
                       
     $actionDataLength = count($actionData);
         
-/*    for ($i = 0; $i < $actionDataLength; ++$i) {
+    for ($i = 0; $i < $actionDataLength; ++$i) {
         $obj = $actionData[$i];
         $actionName = "'" . $obj["actionName"] . "'";
         $time = "'" . $obj["time"] "'";
@@ -42,14 +42,11 @@
         
         $query = "INSERT INTO Action (session_id, action_name, time, time_elapsed, mouse_trace, action_param) VALUES ($sessionId, $actionName, $time, $timeElapsed, $mouseTrace, $actionParam);";
         
-        mysql_query("
-            INSERT INTO Action (session_id, action_name, time, time_elapsed, mouse_trace, action_param)
-            VALUES ($sessionId, $actionName, $time, $timeElapsed, $mouseTrace, $actionParam);
-        ", $con);
-    }
+//        mysql_query($query, $con);
+	}
  
     echo $query;
-    echo mysql_error($con) . "\n"; */
+    echo mysql_error($con) . "\n";
               
     mysql_close($con);
 ?>
