91a6cf8252428f3af5a7ef661f14f64ffb9aa1bd
tacitia <lillian.g621@gmail.com>
2013-04-08 15:20:40 -0400

    remove a bunch of files

--- mTurk/js/main.js
+++
@@ -1,7 +0,0 @@
-var version = '2012-03-25';
-var accessKey = 'AKIAJP4FBQ4FRUTXZCEA';
-var secretKey = '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';
-
-$(document).ready(function() {
-	$('#getAccountBalance').click(getAccountBalance);
-});
