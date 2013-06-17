b6986d8611a5301dbf905755fbd045deffbeab30
tacitia <lillian.g621@gmail.com>
2013-06-17 10:17:46 -0400

    add an "appendsubmitform" function into the util.js

--- tests/util.js
+++ tests/util.js
@@ -4,28 +4,36 @@
 	util.getTurkerID = function() {
 		if (params === undefined) { params = getURLParams(); }
 		return params.workerId;	
-	}
+	};
 	
 	util.getAssignmentID = function() {
 		if (params === undefined) { params = getURLParams(); }
 		return params.assignmentId;
-	}
+	};
 
 	util.getHitID = function() {
 		if (params === undefined) { params = getURLParams(); }
 		return params.hitId;
-	}
+	};
 	
 	
 	util.getSubmitTo = function() {
 		if (params === undefined) { params = getURLParams(); }
 		return params.turkSubmitTo + "/mturk/externalSubmit?assignmentId=" + params.assignmentId;
-	}
+	};
 	
 	util.checkPreviewMode = function() {
 		if (params === undefined) { params = getURLParams(); }
 		return (params.assignmentId == "ASSIGNMENT_ID_NOT_AVAILABLE");
-	}
+	};
+	
+	util.appendSubmitForm = function() {
+		var mturkSubmitForm = $('body').append('div').append('<form></form>');
+		mturkSubmitForm.append('<div><input type="submit" value="Submit"/></div>');
+		mturkSubmitForm.attr('method', 'POST')
+		var submitTo = util.getSubmitTo();
+		mturkSubmitForm.attr('action', submitTo);
+	};
 	
 	function getURLParams() {
 		var params = {};
